5eb448b2fb947d8da0f1e318c24dff92c10116da
sarthak kumar <sarthakkumar10151@gmail.com>
2025-12-09 01:58:43 +0530

    Add bidirectional bfs

--- src/features/algo/index.ts
+++ src/features/algo/index.ts
@@ -1,8 +1,16 @@
 import { PathFinder } from '@/types/algo';
 import { bfs } from './bfs';
 import { dfs } from './dfs';
+import { bidirectionalBfs } from './bidirectionalBfs';
 
 export const algorithms: Record<string, PathFinder> = {
-    bfs,
-    dfs,
+    'Breath-first search': bfs,
+    'Depth-first search': dfs,
+    'Bidirectional BFS': bidirectionalBfs,
 };
+
+export const algoLabels = [
+    'Breath-first search',
+    'Depth-first search', //
+    'Bidirectional BFS',
+];
